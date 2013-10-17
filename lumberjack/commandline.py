f6e2f622e7ede0e7ecd389c044a30a1dd28277e1
Tom Lazar <tomster@pyfidelity.com>
2013-10-17 18:15:05 +0200

    match cli params to api args

--- lumberjack/commandline.py
+++ lumberjack/commandline.py
@@ -12,7 +12,7 @@ def main():
 
 Options:
     -c DIR --config=FILE      The location of the configuration file [default: ./lumberjack.yml]
-    -s DIR --source=DIR       The directory containting the content [default: ./content]
+    -s DIR --content=DIR       The directory containting the content [default: ./content]
     -t DIR --templates=DIR    The directory containting the templates [default: ./templates]
     -d DIR --destination=DIR  The directory that will contain the rendered version [default: ./build]
     --version   Show version
@@ -21,12 +21,12 @@ Options:
     arguments = docopt(main.__doc__, help=True, version=get_distribution('lumberjack').version)
 
     # normalize path arguments
-    for location in ['--config', '--source', '--destination']:
+    for location in ['--config', '--content', '--destination']:
         arguments[location] = abspath(arguments[location])
 
     config = parse_config(arguments['--config'])
-    site = parse_source(arguments['--source'])
-    render_site(site=site, fs_source=arguments['--source'], fs_destination=arguments['--destination'],
+    site = parse_source(arguments['--content'])
+    render_site(site=site, fs_content=arguments['--content'], fs_destination=arguments['--destination'],
         fs_templates=arguments['--templates'],
         config=config)
     print "Done."

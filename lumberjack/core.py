925823fd4595ddab2a884adacfdbba23b5d3d51a
Tom Lazar <tomster@pyfidelity.com>
2013-10-22 12:22:55 +0200

    use propdict for our aspects adapter
    now we just need to subclass it and add properties as needed

--- lumberjack/core.py
+++ lumberjack/core.py
@@ -1,3 +1,4 @@
+from propdict import propdict
 from libstasis import Registry
 from libstasis.entities import IAspects
 from libstasis.entities import Entities
@@ -15,30 +16,39 @@ def factory(config, path):
     site.registerAdapter(AspectsForFile, (File,), IAspects)
     site.registerAdapter(AspectsForRstFile, (RstFile,), IAspects)
     entities = Entities(registry=site)
+    # basic file aspects
     entities.add_aspect('filepath', Column('value', types.Unicode))
+    entities.add_aspect('basepath', Column('value', types.Unicode))
     entities.add_aspect('subpath', Column('value', types.Unicode))
+    entities.add_aspect('localpath', Column('value', types.Unicode))
     entities.add_aspect('filename', Column('value', types.Unicode))
+    # General meta data
+    entities.add_aspect('title', Column('value', types.Unicode))
+    # ReST aspects TODO: factor into module?
     site['entities'] = entities
     return site
 
 
-class AspectsForFile(object):
+class AspectsForFile(propdict):
     implements(IAspects)
 
+    @property
+    def filename(self):
+        return path.split(self.subpath)[1]
+
+    @property
+    def localpath(self):
+        """ returns the path within the source directory,
+        without the filename
+        """
+        return path.split(self.subpath)[0]
+
+    @property
+    def filepath(self):
+        """ returns the absolute path, including the filename
+        """
+        return path.join(self.basepath, self.subpath)
+
     def __init__(self, file):
-        self.file = file
-
-    def __getitem__(self, aspect):
-        if aspect == 'filepath':
-            return self.file.filepath
-        elif aspect == 'subpath':
-            return path.dirname(self.file.subpath)
-        elif aspect == 'filename':
-            return path.split(self.file.subpath)[1]
-        raise KeyError(aspect)
-
-    def keys(self):
-        return iter(['filepath', 'subpath', 'filename'])
-
-    def __iter__(self):
-        return iter(self.keys())
+        self.update(file.__dict__)
+        self.title = u'Welcome'
--- lumberjack/renderer.py
+++ lumberjack/renderer.py
@@ -6,17 +6,16 @@ from shutil import copy2
 def render_site(site, fs_content, fs_destination, fs_templates, config):
     print "Writing to %s" % fs_destination
     env = Environment(loader=FileSystemLoader([fs_templates, fs_content]))
-    for item in site['entities'].query('subpath', 'filename', 'filepath'):
-        fs_item_dir = path.join(fs_destination, item.subpath.value)
+    for item in site['entities'].query('localpath', 'filename', 'filepath', 'subpath'):
+        fs_item_dir = path.join(fs_destination, item.localpath.value)
         if not path.exists(fs_item_dir):
             makedirs(fs_item_dir)
         fs_item_destination = path.join(fs_item_dir, item.filename.value)
         item_type = path.splitext(item.filename.value)[-1].lower()
         if item_type == '.html':
             with open(fs_item_destination, 'w') as destination_file:
-                template = env.get_template(path.join(item.subpath.value, item.filename.value))
+                template = env.get_template(item.subpath.value)
                 destination_file.write(template.render(**config))
-
         else:
             copy2(item.filepath.value, fs_item_destination)
     return fs_destination
